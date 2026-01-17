a93bc149aad664e4fc3089a126300fa2ef2da655
HoangDucBach <hoangbach0985@gmail.com>
2026-01-17 10:30:51 +0700

    fix: allow cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Rylith Indexer API')
     .setDescription('Event indexer for Rylith vaults')
